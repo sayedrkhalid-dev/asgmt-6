0e638ccbe8659bc3caeea5c9ff4e7ccd3cc51e09
sayedrkhalid-dev <sayedrkhalid.official@gmail.com>
2026-03-29 22:35:53 +0600

    slightly completed the Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,42 @@
 import Navbar from "./components/Navbar/Navbar";
 
+const nav_items = [
+  {
+    id: 1,
+    label: "home",
+    path: "/",
+  },
+  {
+    id: 2,
+    label: "products",
+    path: "/products",
+  },
+  {
+    id: 3,
+    label: "features",
+    path: "/features",
+  },
+  {
+    id: 4,
+    label: "pricing",
+    path: "/pricing",
+  },
+  {
+    id: 5,
+    label: "testimonials",
+    path: "/testimonials",
+  },
+  {
+    id: 6,
+    label: "FAQ",
+    path: "/questions",
+  },
+];
+
 function App() {
   return (
     <>
-      <Navbar />
+      <Navbar items={nav_items} />
       <h1 className="text-red-500 text-3xl font-bold">Hello world</h1>
     </>
   );
