06569e4bd2b090cbd3283009dc1545dbb4a02896
sayedrkhalid-dev <sayedrkhalid.official@gmail.com>
2026-03-29 22:08:57 +0600

    installed taiwind, daisyui and configure these

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import Navbar from "./components/Navbar/Navbar";
+
 function App() {
   return (
     <>
-      <h1>Hello world</h1>
+      <Navbar />
+      <h1 className="text-red-500 text-3xl font-bold">Hello world</h1>
     </>
   );
 }
